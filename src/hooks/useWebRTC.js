c4ddadbb30937142241e7bacb04721f16927452e
s2000promax <s2000promax@gmail.com>
2022-10-15 00:46:18 +0400

    second part: add peer

--- src/hooks/useWebRTC.js
+++ src/hooks/useWebRTC.js
@@ -2,6 +2,7 @@ import React, { useCallback, useEffect, useRef } from 'react';
 import useStateWithCallback from './useStateWithCallback';
 import socket from '../socket';
 import ACTIONS from '../socket/actions';
+import freeice from 'freeice';
 
 export const LOCAL_VIDEO = 'LOCAL_VIDEO';
 
@@ -27,6 +28,97 @@ const useWebRTC = (roomID) => {
     }, cb);
   }, [clients, updateClients]);
 
+  // Добавление нового соединения
+  useEffect(() => {
+    // Что мы будем делать, когда добавится новый peer
+    async function handleNewPeer({ peerID, createOffer }) {
+      if (peerID in peerConnection.current) {
+        return console.warn(`Already connection to peer ${peerID}`);
+      }
+
+      peerConnection.current[peerID] = new RTCPeerConnection({
+        iceServers: freeice()
+      })
+
+      // Когда новый iceCandidate желает подключится. handle event. Когда мы сами создаем или offer или answer
+      peerConnection.current[peerID].onicecandidate = (event) => {
+        if (event.candidate) {
+          // если кандидат существует - Пересылаем другим клиентам
+          socket.emit(ACTIONS.RELAY_ICE, {
+            peerID,
+            iceCandidate: event.candidate
+          });
+        }
+      }
+
+      // Извлекаем stream, когда приходит новый track
+      let tracksNumber = 0; // Нам походит два трека - audio и video
+      peerConnection.current[peerID].ontrack = ({ streams: [remoteStream] }) => {
+        tracksNumber += 1; // Увеличиваем каждый раз, как приходит новый track
+
+        if (tracksNumber === 2) { // ожидаем принятия audio и video !!!
+          addNewClient(peerID, () => {
+            // начинаем транслировать remoteStream
+            peerMediaElements.current[peerID].srcObject = remoteStream;
+          });
+        }
+      }
+
+      // Добавляем локальный стрим к нашему peerConnection
+      localMediaStream.current.getTracks().forEach(track => {
+        peerConnection.current[peerID].addTrack(track, localMediaStream);
+      });
+
+      // Если мы сторона, которая создает offer, то
+      if (createOffer) {
+        const offer = await peerConnection.current[peerID].createOffer();
+
+        // Устанавливаем offer как localDescription
+        await peerConnection.current[peerID].setLocalDescription();
+
+        // Отправляем offer
+        socket.emit(ACTIONS.RELAY_SDP, {
+          peerID,
+          sessionDescription: offer
+        });
+      }
+    }
+
+    socket.on(ACTIONS.ADD_PEER, handleNewPeer);
+  }, []);
+
+  // Реагируем на sessionDescription
+  useEffect(() => {
+    async function setRemoteMedia({ peerID, sessionDescription: remoteDescription }) {
+      await peerConnection.current[peerID].setRemoteDescription(
+        new RTCSessionDescription(remoteDescription) // Оборачиваем констуртором RTCSessionDescription
+      );
+
+      if (remoteDescription.type === 'offer') {
+        const answer = await peerConnection.current[peerID].createAnswer();
+
+        await peerConnection.current[peerID].setLocalDescription(answer);
+
+        socket.emit(ACTIONS.RELAY_SDP, {
+          peerID,
+          sessionDescription: answer
+        });
+      }
+    }
+
+    socket.on(ACTIONS.SESSION_DESCRIPTION, setRemoteMedia)
+  }, []);
+
+  // Реагируем на iceCandidate
+  useEffect(() => {
+
+    socket.on(ACTIONS.ICE_CANDIDATE, ({ peerID, iceCandidate }) => {
+      peerConnection.current[peerID].addIceCandidate(
+        new RTCIceCandidate() // Оборачиваем констуртором RTCIceCandidate
+      );
+    });
+  }, []);
+
   // Реагируем на изменение комнаты
   useEffect(() => {
 
@@ -50,10 +142,16 @@ const useWebRTC = (roomID) => {
       });
     }
 
-
     startCapture()
       .then(() => socket.emit(ACTIONS.JOIN, { room: roomID }))
       .catch(e => console.error('Error getting userMedia:', e));
+
+    // Действие при демонтировании
+    return () => {
+      localMediaStream.current.getTracks().forEach(track => track.stop());
+
+      socket.emit(ACTIONS.LEAVE);
+    }
 
   }, [roomID]);
 
